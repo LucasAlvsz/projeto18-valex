185bf8af87aa4f92dae5d3b8fbbd55994f2deeb8
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-07-08 06:26:23 -0300

    chore: fixing types for deploy

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,6 @@ app.get("/", (req, res) => {
 	res.send("Hello World!")
 })
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(+process.env.PORT || 5000, () => {
 	console.log(`Server running on port ${process.env.PORT || 5000}`)
 })
