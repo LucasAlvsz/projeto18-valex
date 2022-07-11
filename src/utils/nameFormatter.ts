549c2a8c74b28b421e541b2902af281c40b33864
Lucas A <contato.lucasalv@gmail.com>
2022-07-11 02:47:05 -0300

    fix: format name function
    now formats correctly and moved to utils folder so it can be used in multiple services

---
+++ src/utils/nameFormatter.ts
@@ -0,0 +1,12 @@
+const nameFormatter = (name: string) => {
+	const nameArray = name.toUpperCase().split(" ")
+	const firstName = nameArray.shift()
+	const lastName = nameArray.pop()
+	let formattedMiddleName = []
+	nameArray.forEach(
+		name => name.length >= 3 && formattedMiddleName.push(name.slice(0, 1))
+	)
+	return `${firstName} ${formattedMiddleName.join(" ")} ${lastName}`
+} // FIXME
+
+export default nameFormatter
