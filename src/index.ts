6f51d668a18027b5fba84bea6d9685c2a1bac659
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-07-12 04:04:01 -0300

    refactor: removing hello world

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,6 @@ app.use(cors())
 app.use(express.json())
 app.use(router)
 
-app.get("/", (req, res) => {
-	res.send("Hello World!")
-})
-
 app.listen(+process.env.PORT || 5000, () => {
 	console.log(`Server running on port ${process.env.PORT || 5000}`)
 })
