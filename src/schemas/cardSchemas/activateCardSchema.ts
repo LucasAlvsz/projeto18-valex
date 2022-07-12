8d9f212944989136a9f287d4ae6f2ff805bc4d6f
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-07-12 18:59:46 -0300

    chore: deploy

--- src/schemas/cardSchemas/activateCardSchema.ts
+++ src/schemas/cardSchemas/activateCardSchema.ts
@@ -3,7 +3,7 @@ import cardIdentifierSchema from "../cardIdentifierSchema"
 
 const bodySchema = cardIdentifierSchema
 	.keys({
-		password: Joi.string().length(4).required(),
+		password: Joi.string().regex(/^\d+$/).length(4).required(),
 		cvc: Joi.string().length(3).required(),
 	})
 	.required()
