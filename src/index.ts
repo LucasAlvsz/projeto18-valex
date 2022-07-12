677350581e3967443ac7b385934b8278d3b69ec9
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-07-12 07:07:28 -0300

    feat: adding the most important part of the project🐱‍👤

--- src/index.ts
+++ src/index.ts
@@ -9,5 +9,5 @@ app.use(express.json())
 app.use(router)
 
 app.listen(+process.env.PORT || 5000, () => {
-	console.log(`Server running on port ${process.env.PORT || 5000}`)
+	console.log(`Server running on port ${process.env.PORT || 5000} 🐱‍👤`)
 })
